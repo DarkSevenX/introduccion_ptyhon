97cae2dfc8923bbb0f62447ae1e8b927d8c3c826
DAOM <diegoospino1535@gmail.com>
2025-03-13 13:38:46 -0500

    Ejercicios 4,5,6

---
+++ cantidad_dinero.py
@@ -0,0 +1,4 @@
+#Se declara una variable de tipo float y se le asigna un valor
+dineroCartera = 150.000
+#Se imprime por pantalla la cantidad de dinero que tengo en mi cartera
+print("La cantidad de dinero que tengo en mi cartera son $",dineroCartera , " mil pesos colombianos") 
---
+++ clima.py
@@ -0,0 +1,5 @@
+#crear una variable de tipo booleano
+gustarClima = False # se declara como falsa
+
+# Se imprime por consola si me gusta el clima caliente o no
+print("Me gusta el clima caliente: ", gustarClima)
